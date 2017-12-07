fdab2ff9dcd97cc86f8f5be03bb6df51c972da01
ArturDNet <artur.dabrowski.net@gmail.com>
2017-12-07 20:57:12 +0100

    few ideas needs to be rethinked

--- src/Game/Main/Board.java
+++ src/Game/Main/Board.java
@@ -8,6 +8,7 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -15,16 +16,29 @@ import javax.swing.Timer;
 public class Board extends JPanel
         implements ActionListener {
 
-    private final int B_WIDTH = 350;
-    private final int B_HEIGHT = 350;
-    private final int INITIAL_X = -100;
-    private final int INITIAL_Y = -100;
-    private final int DELAY = 25;
+    private int randomX = ThreadLocalRandom.current().nextInt(0,630);
+    private int randomY = ThreadLocalRandom.current().nextInt(0,430);
+    private final int B_WIDTH = 750;
+    private final int B_HEIGHT = 550;
+    private int INITIAL_X = 0;
+    private int INITIAL_Y = 0;
+    private final int DELAY = 10;
 
     private Image star;
     private Image background;
     private Timer timer;
     private int x, y;
+    private int xAxis = 1;
+    private int yAxis = 1;
+
+
+    public void setxAxis(int xAxis) {
+        this.xAxis = xAxis;
+    }
+
+    public void setyAxis(int yAxis) {
+        this.yAxis = yAxis;
+    }
 
     public Board() {
 
@@ -47,8 +61,8 @@ public class Board extends JPanel
 
         loadImage();
 
-        x = INITIAL_X;
-        y = INITIAL_Y;
+        x = randomX;
+        y = randomY;
 
         timer = new Timer(DELAY, this);
         timer.start();
@@ -68,17 +82,20 @@ public class Board extends JPanel
         Toolkit.getDefaultToolkit().sync();
     }
 
+    private void direction () {
+        x += xAxis;
+        y += yAxis;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        x += 1;
-        y += 1;
+        direction();
 
-        if (y > B_HEIGHT) {
-
-            y = INITIAL_Y;
-            x = INITIAL_X;
-        }
+        if (y >= B_HEIGHT-120) {setyAxis(-1);}
+        if (x >= B_WIDTH-120) {setxAxis(-1);}
+        if (y == 0) {setyAxis(1);}
+        if (x == 0) {setxAxis(1);}
 
         repaint();
     }
