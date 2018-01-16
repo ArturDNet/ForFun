0a924db7ad819334ffc9eaf9a439f92211d0f8e4
ArturDNet <artur.dabrowski.net@gmail.com>
2018-01-16 18:38:17 +0100

    ball basics

--- src/Game/Main/Ball.java
+++ src/Game/Main/Ball.java
@@ -2,5 +2,41 @@ package Game.Main;
 
 public class Ball extends Game_objects implements Commons {
 
+    private int xMove;
+    private int yMove;
 
+    /* Shape ball = new Ellipse2D.Double(x, y, 10, 10);*/
+    public Ball() {
+        xMove = 1;
+        yMove = -1;
+        width = 10;
+        height = 10;
+        resetBall();
+    }
+
+    private void resetBall() {
+        x = BallInitialX;
+        y = BallInitialY;
+    }
+    public void ballMoves(){
+        x += xMove;
+        y += yMove;
+
+        if (y == 0) {
+            setYMove(1);
+        }
+        if (x == 0) {
+            setXMove(1);
+        }
+        if (x == GameAreaWidth - (width / 2)) {
+            setXMove(-1);
+        }
+    }
+    public void setXMove(int xMove) {
+        this.xMove = xMove;
+    }
+
+    public void setYMove(int yMove) {
+        this.yMove = yMove;
+    }
 }
--- src/Game/Main/Commons.java
+++ src/Game/Main/Commons.java
@@ -7,8 +7,8 @@ public interface Commons {
     public static int LooseBallEdge = 490;  /*10 above the bottom*/
     public static int PaddleInitialX = 370; /*paddle width 60*/
     public static int PaddleInitialY = 470; /*paddle height 10*/
-    public static int BallInitialX = 395;   /*ball radius 10*/
-    public static int BallInitialY = 465;   /*ball radius 10 -> stick to paddle*/
+    public static int BallInitialX = 395;   /*ball radius 5*/
+    public static int BallInitialY = 465;   /*ball radius 5 -> stick to paddle*/
     public static int BlockAmount = 120;    /*block is 20 width and 10 height -> number of blocks = 3 rows of 40 blocks or 4 rows of 30 blocks*/
     public static int Delay = 1000;         /*for timer [ms]*/
     public static int Period = 10;          /*for timer [ms]*/
